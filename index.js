5d861001f7551971f985480df27a7ec94fd6aa47
= <craig@craigshoemaker.net>
2018-09-12 23:48:39 -0400

    Performance improvements

--- index.js
+++ index.js
@@ -21,64 +21,41 @@ if (process.env.NODE_ENV !== "production") {
 const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
 
-const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
-const pipeline = StorageURL.newPipeline(credentials);
-const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
-
 const ONE_MEGABYTE = 1024 * 1024;
 const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
 const ONE_MINUTE = 60 * 1000;
 
-async function listContainers() {
+async function showContainerNames(serviceURL, aborter) {
 
     let response;
     let marker;
-    let containers = [];
-
-    const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
     do {
         response = await serviceURL.listContainersSegment(aborter.withTimeout(ONE_MINUTE), marker);
         marker = response.marker;
-        containers = containers.concat(response.containerItems);
+        response.containerItems.forEach(container => console.log(' - ' + container.name));
     } while (marker);
-
-    return containers;
 }
 
-async function createContainer(name) {
-    const url = ContainerURL.fromServiceURL(serviceURL, name);
-    return await url.create(Aborter.timeout(ONE_MINUTE));
-}
-
-async function uploadString(containerName, blobName, content) {
-    const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blobURL = BlobURL.fromContainerURL(url, blobName);
-    const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
-    return await blockBlobURL.upload(Aborter.timeout(ONE_MINUTE), content, content.length);
-}
-
-async function uploadLocalFile(containerName, filePath) {
+async function uploadLocalFile(containerURL, filePath, aborter) {
 
     filePath = path.resolve(filePath);
 
     const fileName = path.win32.basename(filePath);
 
-    const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blobURL = BlobURL.fromContainerURL(url, fileName);
+    const blobURL = BlobURL.fromContainerURL(containerURL, fileName);
     const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
 
-    return await uploadFileToBlockBlob(Aborter.timeout(ONE_MINUTE), filePath, blockBlobURL);
+    return await uploadFileToBlockBlob(aborter.withTimeout(ONE_MINUTE), filePath, blockBlobURL);
 }
 
-async function uploadStream(containerName, filePath) {
+async function uploadStream(containerURL, filePath, aborter) {
 
     filePath = path.resolve(filePath);
 
     const fileName = path.win32.basename(filePath).replace('.md', '-stream.md');
 
-    const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blobURL = BlobURL.fromContainerURL(url, fileName);
+    const blobURL = BlobURL.fromContainerURL(containerURL, fileName);
     const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
 
     const stream = fs.createReadStream(filePath, {
@@ -91,44 +68,23 @@ async function uploadStream(containerName, filePath) {
     };
 
     return await uploadStreamToBlockBlob(
-                    Aborter.timeout(ONE_MINUTE), 
+                    aborter.withTimeout(ONE_MINUTE), 
                     stream, 
                     blockBlobURL, 
                     uploadOptions.bufferSize, 
                     uploadOptions.maxBuffers);
 }
 
-async function listBlobs(containerName) {
+async function showBlobNames(containerURL, aborter) {
 
     let response;
     let marker;
-    let blobs = [];
 
     do {
-        const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-        response = await url.listBlobFlatSegment(Aborter.timeout(ONE_MINUTE));
+        response = await containerURL.listBlobFlatSegment(aborter.withTimeout(ONE_MINUTE));
         marker = response.marker;
-        blobs = blobs.concat(response.segment.blobItems);
+        response.segment.blobItems.forEach(blob => console.log(' - ' + blob.name));
     } while (marker);
-
-    return blobs;
-}
-
-async function downloadBlob(containerName, blobName) {
-    const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blobURL = BlobURL.fromContainerURL(url, blobName);
-    return await blobURL.download(Aborter.timeout(ONE_MINUTE), 0);
-}
-
-async function deleteBlob(containerName, blobName) {
-    const url = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blobURL = BlobURL.fromContainerURL(url, blobName);
-    return await blobURL.delete(Aborter.timeout(ONE_MINUTE));
-}
-
-async function deleteContainer(name) {
-    const url = ContainerURL.fromServiceURL(serviceURL, name);
-    return await url.delete(Aborter.timeout(ONE_MINUTE));
 }
 
 async function execute() {
@@ -138,37 +94,41 @@ async function execute() {
     const content = "hello!";
     const localFilePath = "./readme.md";
 
+    const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
+    const pipeline = StorageURL.newPipeline(credentials);
+    const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
+    
+    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+    
+    const aborter = Aborter.timeout(30 * ONE_MINUTE);
+
     console.log("Containers:");
-    const containers = await listContainers();
-    containers.forEach((container) => console.log(" - " + container.name));
+    await showContainerNames(serviceURL, aborter);
 
-    const containerDoesNotExist = containers.findIndex((container) => container.name === containerName) === -1;
-    if (containerDoesNotExist) {
-        await createContainer(containerName);
-        console.log(`Container: "${containerName}" is created`);
-    }
+    await containerURL.create(aborter.withTimeout(ONE_MINUTE));
+    console.log(`Container: "${containerName}" is created`);
 
-    await uploadString(containerName, blobName, content);
+    await blockBlobURL.upload(aborter.withTimeout(ONE_MINUTE), content, content.length);
     console.log(`Blob "${blobName}" is uploaded`);
     
-    await uploadLocalFile(containerName, localFilePath);
+    await uploadLocalFile(containerURL, localFilePath, aborter);
     console.log(`Local file "${localFilePath}" is uploaded`);
 
-    await uploadStream(containerName, localFilePath);
+    await uploadStream(containerURL, localFilePath, aborter);
     console.log(`Local file "${localFilePath}" is uploaded as a stream`);
 
     console.log(`Blobs in "${containerName}" container:`);
-    const blobs = await listBlobs(containerName);
-    blobs.forEach((blob) => console.log(" - " + blob.name));
+    await showBlobNames(containerURL, aborter);
 
-    const downloadResponse = await downloadBlob(containerName, blobName);
+    const downloadResponse = await blockBlobURL.download(aborter.withTimeout(ONE_MINUTE), 0);
     const downloadedContent = downloadResponse.readableStreamBody.read(content.length).toString();
     console.log(`Downloaded blob content: "${downloadedContent}"`);
 
-    await deleteBlob(containerName, blobName);
+    blockBlobURL.delete(aborter.withTimeout(ONE_MINUTE))
     console.log(`Block blob "${blobName}" is deleted`);
     
-    await deleteContainer(containerName);
+    containerURL.delete(aborter.withTimeout(ONE_MINUTE));
     console.log(`Container "${containerName}" is deleted`);
 }
 
